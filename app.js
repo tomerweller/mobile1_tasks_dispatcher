10d4b226473fe62ce8b5bd8a90bbf0751cc1148a
Tomer Weller <tomer.weller@gmail.com>
2012-12-23 15:43:38 +0200

    mongo connection

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 var express = require('express')
-	, app = express()
-	, port = process.env.PORT || 5000;
+    , MongoClient = require('mongodb').MongoClient
+    , app = express()
+    , port = process.env.PORT || 5000
+    , tasksCollection = null;
 
-app.get('/random_task', function(req, res){
-  var o = {a: "a", b: "b"}
-  res.send(o);
+MongoClient.connect(process.env.MONGO_DB_URL, function(err, db) {
+    if(!err) {
+        console.log("mongo connection: success");
+        tasksCollection = db.collection('tasks');
+    } else {
+        console.log("mongo connection: err: ", err);
+    }
+});
+
+app.get('/task/random', function(req, res){
+    tasksCollection.count(function(err, count){
+        var random_i = Math.floor(Math.random()*count);
+        console.log('Tasks count is: ' + count, 'Choosing task #' + random_i);
+        //extremely inefficient
+        tasksCollection.find().limit(-1).skip(random_i).toArray(function(err, docs){ 
+            res.send(docs[0]);
+        });
+    });
 });
 
 app.listen(port);
-console.log('Listening on port', port);
+console.log('Listening on port : ', port);
